25deb930c37d71884c7d7077a6b6aa2f1504591b
rkudashev <kudashev.ravil@yandex.ru>
2023-01-25 14:50:31 +0500

    add Client entity

--- app.rb
+++ app.rb
@@ -2,6 +2,13 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:barbershop.db"
+
+class Client < ActineRecord::Base #создали сущность
+
+end
 
 get '/' do
     erb "Yo man!"			
